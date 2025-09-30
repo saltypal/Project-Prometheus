a73eb6365c133fe72f7f88504dc16c3f65596299
Satya Srinivas Paladugu <Satyaspaladugu@gmail.com>
2025-09-30 19:50:06 +0530

    Restructured Code, Introduced a class named Torrent to make the torrent related work simple Refactored entire main.py, created a function named new_torrent which WE MAY USE TO MAKE USE OF MULTIPLE TORRENTS AT A TIME? IDK also our goal torrent is manjaro

--- BT/src/tracker.py
+++ BT/src/tracker.py
@@ -1,5 +1,7 @@
 import hashlib
-
+import requests
+import torrent
+import client
 """
 
 MADE BY SATYA PALADUGU AT 28/9/2025 11:40 PM
@@ -10,14 +12,6 @@ This file deals with:
 1. info_hash
 2. Request generation for HTTP and UDP trackers.
 3. Response acceptance
-
-
-INFO_HASH generation method:
-                            for generating info_hash, i have to make a unique 20-byte SHA1 hash of the bencoded dictionary.
-                            normally, you should bencode back to the ideal bencoded data, and then hash it. 
-                            but this time, we will do something different BECAUSE IM F-ING LAZY.
-                            for this, we will have to pinpoint where the b'info' is in the raw file and then take everything after it.
-
 
 
 key parameters your client must include in the request:
@@ -36,74 +30,18 @@ event: This parameter tells the tracker what's happening. The most common events
 
 class Tracker:
         """
-        This is the tracker class. This class cosns
+        This is the tracker class. This class consists of functions that handle with 
+        1. hash generation.
         """
-        def __init__(self):
-                self.generate_info_hash()
+        def __init__(self, peerID, portNum,rawTorrent,cleanTorrent,info_hash,announceList,sizeOfTorrent,):
 
+                self.peerID = peerID
+                self.portNum = portNum
+                self.info_hash = info_hash
+                self.rawTorrent = rawTorrent
+                self.cleanTorrent = cleanTorrent
+                self.announceList = announceList
+                self.torrentSize = sizeOfTorrent
+                print("Tracker is Ready. Proceeding...")
 
-        def generate_info_hash(self,rawData):
-                """
-                This function is an attempt to create info hash without encoding the whole info_hash.
-
-                for this, we will have to pinpoint where the b'info' is in the raw file and then take everything after it.
-                """
-                word = b'4:info'
-                if rawData:
-                        print("OK Now lets generate info hash using the alt trick.\n Checking where is 4:info.")
-                # reason behind why 4:info, is because the translation of the word 'info' in bencode is '4:info'. SO yes, I predetermined it.
-                try:
-                        # lets try finding info from the main shit first.
-                        position = rawData.index(word)+len(word)
-                        
-                        print("ok i found 4:info. now let's proceed.")
-                except Exception as e:
-                        print("No id didnt find.")
-
-                temp_data = rawData[position:]
-                level = 0
-                end_offset = 0
-                i = 0
-                
-                while i<len(temp_data):
-                        char = temp_data[i:i+1]
-
-                        if char == b'd' or char == b'l': # that means it is a dictionary or a list begining
-                                level +=1
-                        elif char == b'e':
-                                level -=1       
-                        elif char in b'0123456789':
-                                colon_index = temp_data.find(b':',i) # i is the starting position for the search. We don't want to search the whole temp_data every time right so yeah
-                                # colon index would give me the
-                                if colon_index == -1: 
-                                        raise ValueError("Corrupt")
-                                str_len = int(len(temp_data[i:colon_index]) )
-                            # Skip the length, the colon, and the string data itself.
-                                i += (colon_index - i) + str_len
-                        i += 1
-                        if level == 0:
-                                end_offset = i
-                                break
-
-            # If the level is 0, we've found the final closing 'e'.
-                       
-        
-                if end_offset == 0:
-                        raise ValueError("Could not determine the end of the info dictionary.")
-            
-                # Step 3: Slice the raw bytes.
-                self.raw_info_bytes = temp_data[:end_offset]
-                if self.raw_info_bytes: 
-                        # print("I won brother I won, you lost. You lost you damn it you lost")
-                        print("Info dictionary has been extracted.")
-                self.info_hash = self.generate_hash()
-                return self.info_hash
-                
-                
-        
-        def generate_hash(self):
-                # SHA1 always makes 20Byte hashes
-                return hashlib.sha1(self.raw_info_bytes).digest
-
-
-
+       
